1e28833cc12e5346d9f0d98597b87e1f493856ce
mahima98 <mahimaramgolam98@gmail.com>
2023-05-01 21:16:52 +0400

    Added styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from 'astro/config';
 
 import node from "@astrojs/node";
 import auth from 'auth-astro';
+import tailwind from '@astrojs/tailwind';
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     auth(),
+    tailwind()
   ],
   output: "server",
   adapter: node({
